ff37205fa36725855a4556f2fb63d3265908deca
Terri Riggle <codeofarmour@outlook.com>
2018-03-24 16:15:46 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,10 +1,7 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
   my_hash = {"name" => "Terri", "hobby" => "knitting"}
-
-
 end
-
 
 def shipping_manifest
   # set a variable called `the_manifest`, equal to a hash
@@ -15,8 +12,6 @@ def shipping_manifest
     "porcelain vases" => 2,
     "oil paintings" => 3
   }
-
-
 end
 
 def retrieval
@@ -25,10 +20,8 @@ def retrieval
                   "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
-
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
   shipping_manifest["oil paintings"]
-
 end
 
 def adding
@@ -40,14 +33,9 @@ def adding
   # add 2 muskets to the shipping_manifest hash below
   shipping_manifest["muskets"] = 2
 
-
-
   # add 4 gun powder to the shipping_manifest hash below
   shipping_manifest["gun powder"] = 4
 
-
-
   # return the shipping_manifest hash below
-  puts shipping_manifest
-
+  shipping_manifest
 end
